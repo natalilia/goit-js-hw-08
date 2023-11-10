51aedfe59739da7d533977cf66d30810fd9805b0
natalilia <nata.semenii@gmail.com>
2023-11-10 19:20:34 +0100

    vi

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+        player.on(
+            "timeupdate",
+            throttle(function ({ seconds }) {
+              localStorage.setItem("videoplayer-current-time", seconds);
+            }, 1000)
+          );
+          
+          const savedTime = localStorage.getItem("videoplayer-current-time");
+          if (savedTime !== null) {
+            player.setCurrentTime(savedTime);
+          }
+    
