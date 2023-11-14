9305075ace97ddbebca1c3f706c3fff4b18e038d
natalilia <nata.semenii@gmail.com>
2023-11-14 19:39:00 +0100

    hghg

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,44 @@
 import throttle from "lodash.throttle";
-import {saveToLS, loadFromLS} from './helpers';
-const refs = {
-    formElem: document.querySelector('.feedback-form')
+
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input");
+const messageTextarea = document.querySelector("textarea");
+
+const storedFormState =
+  JSON.parse(localStorage.getItem("feedback-form-state")) ?? {};
+emailInput.value = storedFormState.email ?? "";
+messageTextarea.value = storedFormState.message ?? "";
+
+//console.log(storedFormState);
+
+const formState = {
+  email: emailInput.value ?? "",
+  message: messageTextarea.value ?? "",
 };
-refs.formElem.addEventListener('input', onFormInput);
-function onFormInput(e){
-    const key = e.target.name;
-    const value = e.target.value;
-    
-}
-function onLoad(){
-    const name = loadFromLS('name');
-    const message = loadFromLS('message');
-}
-onLoad();
+
+const updateFormState = throttle(() => {
+  localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+}, 500);
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formState[name] = value;
+  updateFormState();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  if (email === "" || message === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  localStorage.removeItem("feedback-form-state");
+  emailInput.value = "";
+  messageTextarea.value = "";
+
+  console.log(formState);
+});
