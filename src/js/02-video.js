36a0c0be0a35db1d00f402966a72845a0e752897
natalilia <nata.semenii@gmail.com>
2023-11-15 18:05:04 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
         player.on(
             "timeupdate",
